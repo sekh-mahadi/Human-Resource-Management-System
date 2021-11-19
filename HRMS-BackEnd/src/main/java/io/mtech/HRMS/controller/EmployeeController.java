8c05bb933acfda5a65edab386f36d7546a372241
Sekh <sekh-mahadi>
2021-11-19 11:52:06 +0600

    Get Employee List: Using REST API

---
+++ HRMS-BackEnd/src/main/java/io/mtech/HRMS/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package io.mtech.HRMS.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mtech.HRMS.model.Employee;
+import io.mtech.HRMS.service.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+}

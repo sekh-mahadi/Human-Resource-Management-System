78417bff22c4ff30fe3886a45e775bd289150388
Sekh <sekh-mahadi>
2021-11-22 13:09:30 +0600

    BackEnd: Add Employee REST Api Completed.

--- HRMS-BackEnd/src/main/java/io/mtech/HRMS/controller/EmployeeController.java
+++ HRMS-BackEnd/src/main/java/io/mtech/HRMS/controller/EmployeeController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.mtech.HRMS.model.Employee;
@@ -22,5 +25,10 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	//create Employee REST api
+	@PostMapping("/employee")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
